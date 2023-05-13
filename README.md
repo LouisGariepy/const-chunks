32ab10bb7695246c02709e174274d86c0aa5860a
Louis Gariépy <louis.gariepy.42@gmail.com>
2023-05-12 20:22:28 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,65 @@
+# `const-chunks`
+
+<div align="center">
+  <!-- Version -->
+  <a href="https://crates.io/crates/const-chunks">
+    <img src="https://img.shields.io/crates/v/const-chunks.svg?style=flat-square"
+    alt="Crates.io version" />
+  </a>
+
+  <!-- Docs -->
+  <a href="https://docs.rs/const-chunks/latest/const-chunks/">
+    <img alt="docs.rs" src="https://img.shields.io/docsrs/const-chunks?style=flat-square">
+  </a>
+  
+  <!-- Dependencies -->
+  <a href="https://deps.rs/repo/github/LouisGariepy/const-chunks">
+    <img src="https://img.shields.io/badge/-no%20dependencies-green?style=flat-square"
+    alt="Crates.io version" />
+  </a>
+ 
+  <!-- License -->
+  <a href="https://github.com/LouisGariepy/const-chunks#License">
+    <img src="https://img.shields.io/badge/License-APACHE--2.0%2FMIT-blue?style=flat-square" alt="License">
+  </a>
+</div>
+
+
+This crate provides an extension trait that lets you chunk iterators into constant-length arrays using `const` generics.
+
+See the [docs](https://docs.rs/const-chunks/latest/const-chunks/) for more info.
+
+```rust
+use const_chunks::IteratorConstChunks;
+
+let v = vec![1, 2, 3, 4, 5, 6];
+let mut v_iter = v.into_iter().const_chunks::<2>();
+assert_eq!(v_iter.next(), Some([1,2]));
+assert_eq!(v_iter.next(), Some([3,4]));
+assert_eq!(v_iter.next(), Some([5,6]));
+```
+
+# Safety
+This crate uses unsafe to manipulate uninitialized memory.
+
+To prevent undefined behaviour, the code runs MIRI in CI, and is both very short and easy to audit.
+
+Nonetheless, you should still consider this fact if you're trying to minimize unsafe dependencies.
+
+## MSRV
+This crate requires `rustc` version 1.65 or newer.
+
+This crate's MSRV is enforced through the manifests `rust-version` key.
+
+## License
+
+Licensed under either of
+ * Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any
+additional terms or conditions.
---
+++ src/lib.rs
@@ -0,0 +1,107 @@
+//! This crate provides an extension trait that lets you chunk iterators into constant-length arrays using `const` generics.
+//!
+//! See the [`IteratorConstChunks::const_chunks`] docs for more info.
+//!
+//! ```rust
+//! use const_chunks::IteratorConstChunks;
+//!
+//! let v = vec![1, 2, 3, 4, 5, 6];
+//! let mut v_iter = v.into_iter().const_chunks::<2>();
+//! assert_eq!(v_iter.next(), Some([1,2]));
+//! assert_eq!(v_iter.next(), Some([3,4]));
+//! assert_eq!(v_iter.next(), Some([5,6]));
+//! ```
+
+use std::mem::MaybeUninit;
+
+/// An iterator that iterates over constant-length chunks, where the length is known at compile time.
+///
+/// This struct is created by the [`IteratorConstChunks::const_chunks`]. See its documentation for more.
+pub struct ConstChunks<const N: usize, I: Iterator> {
+    /// The inner iterator from which we take chunks.
+    inner: I,
+}
+
+impl<const N: usize, I: Iterator> Iterator for ConstChunks<N, I> {
+    type Item = [I::Item; N];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            // Create array of unitialized values
+            let mut arr: [MaybeUninit<I::Item>; N] = MaybeUninit::uninit().assume_init();
+
+            // Initialize items
+            for i in 0..N {
+                // Get the next iterator item
+                let Some(val) = self.inner.next() else {
+                    // If there wasn't enough items to fill the array then
+                    // manually drop the initialized values
+                    for v in &mut arr[..i] {
+                        v.assume_init_drop();
+                    }
+                    // early-return None.
+                    return None;
+                };
+
+                // Initialize item
+                arr[i].write(val);
+            }
+
+            // Cast to an array of definitely initialized `I::Item`s
+            // TODO: use `array_assume_init` when stabilized.
+            let init_arr = (&arr as *const _ as *const [I::Item; N]).read();
+
+            Some(init_arr)
+        }
+    }
+}
+
+/// An extension trait providing [`Iterator`]s with the capability to iterate
+/// over chunks of items.
+pub trait IteratorConstChunks {
+    /// The type of iterator from which we take chunks.
+    type Inner: Iterator;
+
+    /// This function returns an iterator over constant-length chunks of items, where
+    /// the length is provided as a const-generic. This function assumes that the number
+    /// of items can be divided into an integer number of chunks. If there are not
+    /// enough items to fully fill a chunk, then the items are consumed, but no chunk
+    /// will be yielded.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// use const_chunks::IteratorConstChunks;
+    ///
+    /// let v = vec![1, 2, 3, 4, 5, 6];
+    /// let mut v_iter = v.into_iter().const_chunks::<2>();
+    /// assert_eq!(v_iter.next(), Some([1,2]));
+    /// assert_eq!(v_iter.next(), Some([3,4]));
+    /// assert_eq!(v_iter.next(), Some([5,6]));
+    /// ```
+    ///
+    /// When the number of items in the iterator cannot be divided exactly
+    /// into chunks, then the iterator will be fully consumed, but the last
+    /// chunk will not be yielded.
+    /// ```
+    /// use const_chunks::IteratorConstChunks;
+    ///
+    /// let v = (1..=5).map(|n| n.to_string()).collect::<Vec<String>>(); // Five items cannot fit into chunks of length 2!
+    /// let mut v_iter = v.into_iter().const_chunks::<2>();
+    /// assert_eq!(v_iter.next(), Some([String::from("1"),String::from("2")]));
+    /// assert_eq!(v_iter.next(), Some([String::from("3"),String::from("4")]));
+    /// assert_eq!(v_iter.next(), None); // `None`, even though there was still one item
+    /// ```
+    fn const_chunks<const N: usize>(self) -> ConstChunks<N, Self::Inner>;
+}
+
+/// Blanket implementation over all iterators.
+impl<I: Iterator> IteratorConstChunks for I {
+    type Inner = Self;
+
+    fn const_chunks<const N: usize>(self) -> ConstChunks<N, Self::Inner> {
+        ConstChunks { inner: self }
+    }
+}
